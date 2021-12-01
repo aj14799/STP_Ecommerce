9df6b55aef77a7cc3642c8b06d8dcd841abbc7cb
Aditya Jha <54480232+aj14799@users.noreply.github.com>
2021-12-02 00:00:12 +0530

    Update cart-item.component.jsx

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -7,7 +7,7 @@ function CartItem({ item: { imageUrl, price, name, quantity } }) {
             <ImageHolder src={imageUrl} alt={name} />
             <ItemDetailsContainer>
                 <NameSpanner>{name}</NameSpanner>
-                <QuantitySpanner>{quantity}* ${price}</QuantitySpanner>
+                <QuantitySpanner>{quantity}* Rs {price*74.3}</QuantitySpanner>
             </ItemDetailsContainer>
         </CartItemContainer>
     )
